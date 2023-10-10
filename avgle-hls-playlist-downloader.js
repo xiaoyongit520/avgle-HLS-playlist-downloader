4acf489cf1f9b49d945fa3533837f927f0f2fbf5
ken <youyouxiaoyong@163.com>
2023-10-10 20:54:11 +0800

    call webapi

--- avgle-hls-playlist-downloader.js
+++ avgle-hls-playlist-downloader.js
@@ -1,195 +1,240 @@
+// ==UserScript==
 // @name         avgle HLS playlist downloader
-// @version      0.1.8
+// @namespace    https://avgle.com/
+// @version      0.1.9
 // @description  decrypts and downloads avgle HLS playlist in browser
-// @author       avotoko
+// @author       avotoko by edit ken
+// @match        https://avgle.com/*
+// ==/UserScript==
 
-(function(){
-	"use strict";
-	let d = document, ver = "v.0.1.8";
-	
-	function info(msg)
-	{
-		let e = d.querySelector('div.ahpd-info');
-		e && (e.textContent = msg);
-	}
-	
-	function log()
-	{
-		console.log.apply(console,["[avgleHPD]"].concat(Array.from(arguments)));
-	}
-	
-	function loginfo()
-	{
-		log.apply(console,arguments);
-		info.apply(console,arguments);
-	}
-	
-	function appendStylesheet(rules, id)
-	{
-		let e = d.createElement("style");
-		if (id)
-			e.id = id;
-		e.type = "text/css";
-		e.innerHTML = rules;
-		d.getElementsByTagName("head")[0].appendChild(e);
-	}
-	
-	function downloadPlaylist(playlist, filename)
-	{
-		if (typeof avglehpdPreDownload === "function"){
-			let r = avglehpdPreDownload({playlist});
-			filename = (r && r.filename) || filename;
-		}
-		let a = d.querySelector('.ahpd-download');
-		a.href = URL.createObjectURL(new Blob([playlist],{type: "application/x-mpegURL"}));
-		a.setAttribute("download",filename);
-		a.classList.remove("ahpd-hide");
-	}
+(function () {
+    "use strict";
+    let d = document, ver = "v.0.1.9";
 
-	function isSegmentUriEncrypted(playlist)
-	{
-		let a = playlist.split('\n');
-		for (let i = 0 ; i < a.length ; i++){
-			if (/^\s*$/.test(a[i]))
-				continue;
-			if (a[i].charAt(0) === "#"){
-				let tag = a[i];
-				if (/^#EXT-X-ENDLIST/.test(tag))
-					break;
-				continue;
-			}
-			let uri = a[i];
-			if (uri.includes('!'))
-				return true;
-		}
-		return false;
-	}
+    function info(msg) {
+        let e = d.querySelector('div.ahpd-info');
+        e && (e.textContent = msg);
+    }
 
-	function decryptPlaylist(playlist, options)
-	{
-		let a = playlist.split('\n');
-		for (let i = 0 ; i < a.length ; i++){
-			if (/^\s*$/.test(a[i]))
-				continue;
-			if (a[i].charAt(0) === "#"){
-				let tag = a[i];
-				if (/^#EXT-X-ENDLIST/.test(tag))
-					break;
-				continue;
-			}
-			let uri = a[i];
-			if (uri.includes("!")){
-				options.uri = uri;
-				options.decryptURI();
-				if (! /^https:\/\//.test(options.uri)){
-					log("can't decript uri:",uri);
-					throw Error("can't decrypt uri");
-				}
-				a[i] = options.uri;
-			}
-		}
-		return a.join('\n');
-	}
-	
-	function main()
-	{
-		if (! videojs){
-			throw new Error("videojs not defined");
-		}
-		window.md5 = new Proxy(window.md5, {
-			apply: function(target, thisArg, argumentsList) {
-				if (/(^avgle\.com\/|\/avgle-hls-playlist-downloader\.js$)/.test(argumentsList[0])){
-					argumentsList[0] = "avgle.com/templates/frontend/videojs-contrib-hls.js";
-				}
-				return Reflect.apply(target, thisArg, argumentsList);
-			}
-		});
-		let prevBeforeRequest = videojs.Hls.xhr.beforeRequest;
-		function restoreBeforeRequest()
-		{
-			videojs.Hls.xhr.beforeRequest = prevBeforeRequest;
-			log("restored videojs.Hls.xhr.beforeRequest");
-		}
-		videojs.Hls.xhr.beforeRequest = function (options) {
-			log("beforeRequest:",options.uri);
-			if (/\/(video)?playback/.test(options.uri)) {
-				log("got target request:",options.uri);
-				setTimeout(function () {
-					log("hooking request callback");
-					info("wating http response");
-					let prevCallback = options.callback;
-					options.callback = function(error,request){
-						loginfo("got response");
-						if (request.rawRequest.response.includes('#EXTM3U')){
-							let playlist = request.rawRequest.response;
-							loginfo("got hls playlist");
-							if (isSegmentUriEncrypted(playlist)){
-								loginfo("segment uri is encrypted");
-								let newOptions = videojs.Hls.xhr.beforeRequest({uri:"!dummy"});
-								if (typeof newOptions.decryptURI !== "function")
-									throw new Error("can't retrieve decryptURI function");
-								log("decryptURI:\n",newOptions.decryptURI.toString());
-								loginfo("decrypting uri in playlist");
-								playlist = decryptPlaylist(playlist, newOptions);
-								log("decrypted playlist:\n"+ playlist);
-								info("decrypted playlist successfully");
-								downloadPlaylist(playlist, "avgle.m3u8");
-							}
-							else {
-								log("segment uri is not encrypted");
-								downloadPlaylist(playlist, "avgle.m3u8");
-							}
-						}
-						else {
-							loginfo("error: can't decrypt response!");
-							log("avgle-main-ah.js must already decrypt the response if the response is encrypted");
-						}
-						if (prevCallback)
-							prevCallback(error,request);
-					};
-				},0);
-				setTimeout(restoreBeforeRequest, 0);
-			}
-			return prevBeforeRequest(options);
-		};
-		log("hooked videojs.Hls.xhr.beforeRequest and waiting hls xhr request");
-		info("Please click the close button.");
-		d.querySelector("#player_3x2_container").addEventListener("click",()=>{
-			info("waiting hls xhr request");
-			log("the close button clicked");
-		});
-		log("waiting for the close button to be clicked");
-	}
-	try {
-		if (d.querySelector(".ahpd-area")){
-			alert("avgleHPD already executed");
-			return;
-		}
-		log("avgle HLS playlist downloader "+ver);
-		console.clear = function(){};
-		{
-			let s, e, sel = "div.container > div.row";
-			if (! (e = d.querySelector(sel))){
-				log("element '"+sel+"' not found");
-				alert("avgleHPD error: "+"element '"+sel+"' not found");
-				return;
-			}
-			appendStylesheet(".ahpd-area{display:flex; font-size:large; }.ahpd-ver{margin-right:5px; background-color:gold; font-weight:bold; text-align:center; vertical-align:middle; border:1px solid transparent; padding:8px 12px; width:min-content; white-space:nowrap; border-radius:4px; }.ahpd-info{margin-right:5px; background-color:beige; text-align:center; border:1px solid transparent; padding:8px 12px; width:min-content; white-space:nowrap; font-size:large; border-radius:4px; }.ahpd-download{font-weight:bold; padding:8px 12px; }.ahpd-download:hover{border:1px outset transparent; } .ahpd-hide{display:none;}");
-			let area = e.insertBefore(d.createElement("div"), e.firstElementChild);
-			area.className = "ahpd-area";
-			e = area.appendChild(d.createElement("div"));
-			e.className = "ahpd-ver";
-			e.textContent = "avgleHPD " + ver;
-			e = area.appendChild(d.createElement("div"));
-			e.className = "ahpd-info";
-			e.textContent = "avgleHPD information here";
-			e = area.appendChild(d.createElement("a"));
-			e.className = "btn-primary ahpd-download ahpd-hide";
-			e.textContent = "Download HLS Playlist";
-		}
-		main();
-	}
-	catch(e){
-		loginfo("error: " + e.message);
-	}
+    function log() {
+        console.log.apply(console, ["[avgleHPD]"].concat(Array.from(arguments)));
+    }
+
+    function loginfo() {
+        log.apply(console, arguments);
+        info.apply(console, arguments);
+    }
+
+    function appendStylesheet(rules, id) {
+        let e = d.createElement("style");
+        if (id)
+            e.id = id;
+        e.type = "text/css";
+        e.innerHTML = rules;
+        d.getElementsByTagName("head")[0].appendChild(e);
+    }
+
+    function downloadPlaylist(playlist, filename) {
+        if (typeof avglehpdPreDownload === "function") {
+            let r = avglehpdPreDownload({playlist});
+            filename = (r && r.filename) || filename;
+        }
+        // let a = d.querySelector('.ahpd-download');
+        /*		a.href = URL.createObjectURL(new Blob([playlist], { type: "application/x-mpegURL" }));
+                a.setAttribute("download", filename);
+                a.classList.remove("ahpd-hide");*/
+        // 实例化一个Request实例
+// 第一个参数一般指资源路径
+// 第二个参数可以理解为请求的配置项，包含头部信息和http请求一些关键配置（请求类型、参数...）
+        let requestInstance = new Request('http://127.0.0.1:3800/', {
+            method: 'post',
+            headers: {
+                'Content-Type': 'application/json;charset=utf-8'
+            },
+            body: {m3u8: playlist, fileName: filename}
+        })
+// fetch方法参数同Request实例
+// 第一个参数为url或者Request实例
+// 第二个参数为请求配置项
+        fetch(requestInstance).then(response => {
+            // 返回的是一个Response的实例
+            // 调用Response实例的序列化方法，序列化成json,返回值是一个promise
+            // 序列化方法有 json,text,formData,blob,arrayBuffer,redirct
+            let result = response.json()
+            result.then(res => {
+                consolee.log(res)
+            })
+        })
+
+    }
+
+    function isSegmentUriEncrypted(playlist) {
+        let a = playlist.split('\n');
+        for (let i = 0; i < a.length; i++) {
+            if (/^\s*$/.test(a[i]))
+                continue;
+            if (a[i].charAt(0) === "#") {
+                let tag = a[i];
+                if (/^#EXT-X-ENDLIST/.test(tag))
+                    break;
+                continue;
+            }
+            let uri = a[i];
+            if (uri.includes('!'))
+                return true;
+        }
+        return false;
+    }
+
+    function decryptPlaylist(playlist, options) {
+        let a = playlist.split('\n');
+        for (let i = 0; i < a.length; i++) {
+            if (/^\s*$/.test(a[i]))
+                continue;
+            if (a[i].charAt(0) === "#") {
+                let tag = a[i];
+                if (/^#EXT-X-ENDLIST/.test(tag))
+                    break;
+                continue;
+            }
+            let uri = a[i];
+            if (uri.includes("!")) {
+                options.uri = uri;
+                options.decryptURI();
+                if (!/^https:\/\//.test(options.uri)) {
+                    log("can't decript uri:", uri);
+                    throw Error("can't decrypt uri");
+                }
+                a[i] = options.uri;
+            }
+        }
+        return a.join('\n');
+    }
+
+    function main() {
+        if (!videojs) {
+            throw new Error("videojs not defined");
+        }
+        window.md5 = new Proxy(window.md5, {
+            apply: function (target, thisArg, argumentsList) {
+                if (/(^avgle\.com\/|\/avgle-hls-playlist-downloader\.js$)/.test(argumentsList[0])) {
+                    argumentsList[0] = "avgle.com/templates/frontend/videojs-contrib-hls.js";
+                }
+                return Reflect.apply(target, thisArg, argumentsList);
+            }
+        });
+
+        function clipboardWrite(value) {
+            window.navigator.clipboard.writeText(value)
+                .then(() => {
+                    console.log('Text copied to clipboard');
+                })
+                .catch(err => {
+                    // This can happen if the user denies clipboard permissions:
+                    console.error('Could not copy text: ', err);
+                });
+        };
+
+        function getVideoTitle() {
+            let title = 'avgle';
+            title = decodeURI(window.location.pathname);
+            title = title.replace('/video/', '');
+            title = title.replaceAll('/', '-');
+            title = title.replace(/\s*/g, '');
+            log(title);
+            return title;
+
+        };
+        log(getVideoTitle());
+        let prevBeforeRequest = videojs.Hls.xhr.beforeRequest;
+
+        function restoreBeforeRequest() {
+            videojs.Hls.xhr.beforeRequest = prevBeforeRequest;
+            log("restored videojs.Hls.xhr.beforeRequest");
+        }
+
+        videojs.Hls.xhr.beforeRequest = function (options) {
+            log("beforeRequest:", options.uri);
+            if (/\/(video)?playback/.test(options.uri)) {
+                log("got target request:", options.uri);
+                setTimeout(function () {
+                    log("hooking request callback");
+                    info("wating http response");
+                    let prevCallback = options.callback;
+                    options.callback = function (error, request) {
+                        loginfo("got response");
+
+                        if (request.rawRequest.response.includes('#EXTM3U')) {
+                            let playlist = request.rawRequest.response;
+                            loginfo("got hls playlist");
+                            let title = getVideoTitle();
+                            clipboardWrite(title);
+                            if (isSegmentUriEncrypted(playlist)) {
+                                loginfo("segment uri is encrypted");
+                                let newOptions = videojs.Hls.xhr.beforeRequest({uri: "!dummy"});
+                                if (typeof newOptions.decryptURI !== "function")
+                                    throw new Error("can't retrieve decryptURI function");
+                                log("decryptURI:\n", newOptions.decryptURI.toString());
+                                loginfo("decrypting uri in playlist");
+                                playlist = decryptPlaylist(playlist, newOptions);
+                                log("decrypted playlist:\n" + playlist);
+                                info("decrypted playlist successfully");
+                                downloadPlaylist(playlist, `${title}.m3u8`);
+                            } else {
+                                log("segment uri is not encrypted");
+                                downloadPlaylist(playlist, `${title}.m3u8`);
+                            }
+                        } else {
+                            loginfo("error: can't decrypt response!");
+                            log("avgle-main-ah.js must already decrypt the response if the response is encrypted");
+                        }
+                        if (prevCallback)
+                            prevCallback(error, request);
+                    };
+                }, 0);
+                setTimeout(restoreBeforeRequest, 0);
+            }
+            return prevBeforeRequest(options);
+        };
+        log("hooked videojs.Hls.xhr.beforeRequest and waiting hls xhr request");
+        info("Please click the close button.");
+        d.querySelector("#player_3x2_container").addEventListener("click", () => {
+            info("waiting hls xhr request");
+            log("the close button clicked");
+        });
+        log("waiting for the close button to be clicked");
+    }
+
+    try {
+        if (d.querySelector(".ahpd-area")) {
+            alert("avgleHPD already executed");
+            return;
+        }
+        log("avgle HLS playlist downloader " + ver);
+        console.clear = function () {
+        };
+        {
+            let s, e, sel = "div.container > div.row";
+            if (!(e = d.querySelector(sel))) {
+                log("element '" + sel + "' not found");
+                alert("avgleHPD error: " + "element '" + sel + "' not found");
+                return;
+            }
+            appendStylesheet(".ahpd-area{display:flex; font-size:large; }.ahpd-ver{margin-right:5px; background-color:gold; font-weight:bold; text-align:center; vertical-align:middle; border:1px solid transparent; padding:8px 12px; width:min-content; white-space:nowrap; border-radius:4px; }.ahpd-info{margin-right:5px; background-color:beige; text-align:center; border:1px solid transparent; padding:8px 12px; width:min-content; white-space:nowrap; font-size:large; border-radius:4px; }.ahpd-download{font-weight:bold; padding:8px 12px; }.ahpd-download:hover{border:1px outset transparent; } .ahpd-hide{display:none;}");
+            let area = e.insertBefore(d.createElement("div"), e.firstElementChild);
+            area.className = "ahpd-area";
+            e = area.appendChild(d.createElement("div"));
+            e.className = "ahpd-ver";
+            e.textContent = "avgleHPD " + ver;
+            e = area.appendChild(d.createElement("div"));
+            e.className = "ahpd-info";
+            e.textContent = "avgleHPD information here";
+            e = area.appendChild(d.createElement("a"));
+            e.className = "btn-primary ahpd-download ahpd-hide";
+            e.textContent = "Download HLS Playlist";
+        }
+        main();
+    } catch (e) {
+        loginfo("error: " + e.message);
+    }
 })();
